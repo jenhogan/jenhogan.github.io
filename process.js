16f231d15b84540734357899adb9558731419f49
Lucas Biagini <lucasbiagini@live.com>
2015-10-07 13:32:24 -0400

    Delete process.js

--- process.js
+++
@@ -1,8 +0,0 @@
-var other = document.getElementById("other");
-var otherField = document.getElementById("other-field");
-
-while(1 == 1)
-{
-	if (other.checked == false) otherField.disabled = true;
-	else otherField.disabled = false;
-}
