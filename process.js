62336192f0b83ee9c4da87f84c6b24ba8cc17223
Lucas Biagini <lbs4@cin.ufpe.br>
2015-10-07 13:27:52 -0400

    registration.html and survey.html files

---
+++ process.js
@@ -0,0 +1,8 @@
+var other = document.getElementById("other");
+var otherField = document.getElementById("other-field");
+
+while(1 == 1)
+{
+	if (other.checked == false) otherField.disabled = true;
+	else otherField.disabled = false;
+}
