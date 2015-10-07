064267463bda71770fa165df6289546783bc3741
jenhogan <jl18hoga@siena.edu>
2015-10-07 15:09:59 -0400

    All of Jen's files
    help

---
+++ fun.js
@@ -0,0 +1,19 @@
+vanillaSlideshow.init({
+  // auto slideshow, options: true or false
+  slideshow: true,
+
+  // slideshow delay
+  delay: 5000,
+
+  // display arrows, options: true or false
+  arrows: true,
+
+  // display indicators, options: true or false
+  indicators: true,
+
+  // start slideshow randomly, options: true or false
+  random: false,
+
+  // animation speed
+  animationSpeed: '1s'
+});
